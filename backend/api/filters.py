e57b942589e3ea7571b0535dde9dea7f1dae65fd
Zimin Denis <zetic@rambler.ru>
2023-05-31 06:14:11 +0300

    test recipefilter

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -11,31 +11,11 @@ class IngredientFilter(SearchFilter):
     search_param = 'name'
 
 
-# class RecipeFilter(django_filters.FilterSet):
-#     is_favorited = django_filters.BooleanFilter(method='filter_is_favorited')
-#     is_in_shopping_cart = django_filters.BooleanFilter(
-#         method='filter_is_in_shopping_cart'
-#     )
-#     tags = django_filters.AllValuesMultipleFilter(field_name='tags__slug')
-#     author = django_filters.ModelChoiceFilter(
-#         queryset=User.objects.all()
-#     )
-
-#     class Meta:
-#         model = Recipes
-#         fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
-
-#     def filter_is_favorited(self, queryset, name, value):
-#         if value and self.request.user.is_authenticated:
-#             return queryset.filter(favorite__user=self.request.user)
-#         return queryset
-
-#     def filter_is_in_shopping_cart(self, queryset, name, value):
-#         if value and self.request.user.is_authenticated:
-#             return queryset.filter(shopping_cart__user=self.request.user)
-#         return queryset
-
 class RecipeFilter(django_filters.FilterSet):
+    is_favorited = django_filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = django_filters.BooleanFilter(
+        method='filter_is_in_shopping_cart'
+    )
     tags = django_filters.AllValuesMultipleFilter(field_name='tags__slug')
     author = django_filters.ModelChoiceFilter(
         queryset=User.objects.all()
@@ -44,3 +24,23 @@ class RecipeFilter(django_filters.FilterSet):
     class Meta:
         model = Recipes
         fields = ('tags', 'author',)
+
+    def filter_is_favorited(self, queryset, name, value):
+        if value and self.request.user.is_authenticated:
+            return queryset.filter(favorite__user=self.request.user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if value and self.request.user.is_authenticated:
+            return queryset.filter(shopping_cart__user=self.request.user)
+        return queryset
+
+# class RecipeFilter(django_filters.FilterSet):
+#     tags = django_filters.AllValuesMultipleFilter(field_name='tags__slug')
+#     author = django_filters.ModelChoiceFilter(
+#         queryset=User.objects.all()
+#     )
+
+#     class Meta:
+#         model = Recipes
+#         fields = ('tags', 'author',)
