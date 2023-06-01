d865e403711095707d3a40d928fc810b87284db7
Zimin Denis <zetic@rambler.ru>
2023-06-01 08:20:32 +0300

    favoriteS in filter

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -23,7 +23,7 @@ class RecipeFilter(django_filters.FilterSet):
 
     class Meta:
         model = Recipes
-        fields = ('tags', 'author',)
+        fields = ('tags', 'author', 'is_favorited','is_in_shopping_cart')
 
     def filter_is_favorited(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
